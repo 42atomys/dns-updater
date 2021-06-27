586838bac2c727053b12802834e7256f8eccd529
Gregory DALMAR <contact@atomys.fr>
2021-06-27 16:43:16 +0200

    first commit

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "os"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const WebhookURL = "https://canary.discord.com/api/webhooks/858713820200566804/NbsedN-G2yzbtM2vM9TyKXODYe4Jw0HVtC_AcZxPk9yTsqA5LhBsAxsBo23SYFJ0hKmK"
+
+type Content struct {
+	Content  string `json:"content"`
+	Username string `json:"username"`
+}
 
 func main() {
-	os.Exit(0)
+
+	log.Println("Getting current IP")
+	resp, err := http.Get("https://ifconfig.co/ip")
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := Content{
+		Content:  fmt.Sprintf("IP: %s", string(bodyBytes)),
+		Username: "DNS Updater",
+	}
+
+	var jsonData []byte
+	jsonData, err = json.Marshal(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Post to Discord")
+	_, err = http.Post(WebhookURL, "application/json", bytes.NewReader(jsonData))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Done !")
 }
+
+//  https://discord.com/api/webhooks/858713820200566804/NbsedN-G2yzbtM2vM9TyKXODYe4Jw0HVtC_AcZxPk9yTsqA5LhBsAxsBo23SYFJ0hKmK
